5907fb30465dd9faab7b87ef1437c7995702a1df
chris <bbhriss@gmail.com>
2025-03-19 17:41:27 -0700

    new changes

--- algo1.py
+++ algo1.py
@@ -4,8 +4,22 @@ Algo 1 by Chris Alpuerto
 '''
 
 
-def WallsOfMaria(input_matrix):
-    return "test"
+class WallsOfMaria:
+
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+    def SafeCity(self, inp_matrix):
+        '''
+        -1 = titan infested area that is impoassable
+        0 = safe stronghold that can take refuge
+        INF = An open city where humans are trying to survive
+        
+        '''
+        directions = [(0,1),(0,-1),(1,0),(-1,0)]
+        return 'test'
+    
+
 
 
 
